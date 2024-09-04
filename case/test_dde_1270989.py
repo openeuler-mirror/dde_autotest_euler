f1379a1a54bf2325b6393988f9145cdee1415e42
ut005790 <lihaobo@uniontech.com>
2024-09-04 17:06:36 +0800

    新增一条用例，用于检查手动在控制中心中修改时间功能

---
+++ case/test_dde_1270989.py
@@ -0,0 +1,25 @@
+from case.base_case import BaseCase
+from method.dde_method import DdeMethod
+from youqu3 import sleep
+from method.dde_control_center_method import DdeControlCenterMethod
+
+
+class TestDdeCase(BaseCase):
+    def test_dde_1270989(self):
+        """控制中心中手动修改时间"""
+        euler = DdeMethod()
+        euler.dde_dock_method_click_control_center_btn_by_attr()
+        sleep(6)
+        euler.dde_method_change_time_by_control_center()
+        sleep(2)
+        self.assert_image_exist_in_dde("test_dde_1270989_1.png")
+        sleep(2)
+        self.assert_image_exist_in_dde("test_dde_1270989_2.png")
+
+    def teardown_method(self):
+        """重新开启时间同步，重置时间"""
+        DdeMethod().dde_control_center_method_click_time_setting_by_attr()
+        sleep(1)
+        DdeMethod().dde_control_center_method_click_time_synchronization_btn_by_attr()
+        sleep(2)
+        DdeMethod().dde_method_close_window()
