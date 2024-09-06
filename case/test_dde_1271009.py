0df7d22584ba542c171526bc0804cddaa80993a9
Harbor <lihaobo@uniontech.com>
2024-09-06 11:25:25 +0800

    新增一条用例，检查在控制中心更改屏幕分辨率后能否成功更换

---
+++ case/test_dde_1271009.py
@@ -0,0 +1,17 @@
+from case.base_case import BaseCase
+from method.dde_method import DdeMethod
+from pylinuxauto import sleep
+
+
+class TestDdeCase(BaseCase):
+    def test_dde_1271009(self):
+        """控制中心中更改屏幕分辨率后查看是否成功更换"""
+        DdeMethod().dde_dock_method_click_control_center_btn_by_attr()
+        sleep(6)
+        DdeMethod().dde_method_change_resolution_by_control_center()
+        self.assert_image_exist_in_dde("test_dde_1271009.png")
+
+    def teardown_method(self):
+        """将分辨率重置，关闭控制中心窗口"""
+        DdeMethod().dde_method_reset_resolution_by_control_center()
+        DdeMethod().dde_method_close_window()
