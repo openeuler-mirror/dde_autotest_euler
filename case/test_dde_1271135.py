b3831c6f563ec7ac5b6b7015d37a3410672a2ea5
Harbor <lihaobo@uniontech.com>
2024-09-09 13:06:59 +0800

    新增一条用例，检查文本编辑器对修改内容的保存功能

--- case/test_dde_1271135.py
+++ case/test_dde_1271135.py
@@ -8,8 +8,9 @@ class TestDdeCase(BaseCase):
     def test_dde_1271135(self):
         """在浏览器中新打开网页，并且进入百度界面"""
         DdeMethod().dde_method_open_software_by_launcher("Firefox")
-        sleep(6)
+        sleep(10)
         DdeMethod().dde_browser_method_right_click_by_ocr("开源社区")
+        sleep(3)
         self.assert_image_exist_in_dde("test_dde_1271135_1.png")
         DdeMethod().dde_browser_method_click_by_attr("新建标签页")
         DdeMethod().dde_browser_method_click_by_attr("使用 百度 搜索，或者输入网址")
