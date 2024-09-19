c7838e19dbf8358ffb246368c14a6decab2e6965
xietian <xietian@uniontech.com>
2024-09-19 19:39:21 +0800

    新增

---
+++ case/test_dde_1271153.py
@@ -0,0 +1,18 @@
+from pylinuxauto import sleep
+from case.base_case import BaseCase
+from method.dde_method import DdeMethod
+import pylinuxauto
+
+
+class TestDdeCase(BaseCase):
+    def test_dde_1271153(self):
+        """归档管理器"""
+        euler = DdeMethod()
+        euler.dde_method_open_software_by_launcher("guidang")
+        sleep(2)
+        self.assert_process_status(True, 'deepin-compressor')
+        pylinuxauto.alt_f4()
+
+    def teardown_method(self):
+        """"""
+
