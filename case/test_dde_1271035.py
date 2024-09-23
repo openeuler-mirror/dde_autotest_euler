e93306f810333255e44401a4550c37c77f0ce3a5
qicoffee <qisijia@uniontech.com>
2024-09-23 10:12:24 +0800

    新增一条用例，用于测试在控制中心中删除普通账户（非当前登陆）

--- case/test_dde_1271035.py
+++ case/test_dde_1271035.py
@@ -3,15 +3,11 @@ from method.dde_method import DdeMethod
 
 
 class TestDdeCase(BaseCase):
-    def test_dde_1271035_1(self):
+    def test_dde_1271035(self):
         """在控制中心中新建标准账户"""
         DdeMethod().dde_method_add_common_account_by_control_center()
         self.assert_image_exist_in_dde("test_dde_1271035_1.png")
 
-    def test_dde_1271035_2(self):
-        """在控制中心中新建管理员账户"""
-        DdeMethod().dde_method_add_root_account_by_control_center()
-        self.assert_image_exist_in_dde("test_dde_1271035_2.png")
 
     def teardown_method(self):
         """删除新增账户并且关闭控制中心"""
