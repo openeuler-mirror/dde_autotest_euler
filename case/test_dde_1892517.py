4907753992de5ec14d9b3707d218965b0de1265e
mataotao <mataotao@uniontech.com>
2025-06-12 10:55:47 +0800

    新增用例test_dde_1892517.py::test_dde_1892517_2

--- case/test_dde_1892517.py
+++ case/test_dde_1892517.py
@@ -17,6 +17,29 @@ class TestDdeCase(BaseCase):
         self.assert_process_status(True, "deepin-devicemanager")
 
     def test_dde_1892517_2(self):
+        """桌面打开设备管理器"""
+        euler = DdeMethod()
+        euler.dde_dock.click_launcher_btn_by_attr()
+        euler.dde_launcher.click_search_box_by_attr()
+        Src.input("shebeiguanliqi")
+        sleep(3)
+        euler.dde_launcher.right_click_by_attr("设备管理器")
+        Src.select_menu(2)
+        sleep(1)
+        Src.esc()
+        euler.click_restore()
+        sleep(1)
+        euler.dde_dock.double_click_by_img("deepin_devicemanager_icon.png")
+        sleep(5)
+        self.assert_process_status(True, "deepin-devicemanager")
+        DdeMethod().kill_process("deepin-devicemanager")
+        sleep(3)
+        euler.dde_dock.right_click_by_img("deepin_devicemanager_icon.png")
+        Src.select_menu(1)
+        sleep(5)
+        self.assert_process_status(True, "deepin-devicemanager")
+
+    def test_dde_1892517_3(self):
         """终端打开设备管理器"""
         Cmd.run_cmd("deepin-devicemanager &")
         sleep(5)
