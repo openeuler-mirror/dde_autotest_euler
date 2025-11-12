482e544a4525ce5f73995528edd299ba0d7ffcb4
lixintao <lixintao@uniontech.com>
2025-11-12 11:17:10 +0800

    新增用例test_dde_1952383.py

---
+++ case/test_dde_1952383.py
@@ -0,0 +1,31 @@
+from time import sleep
+from src import Src
+import pytest
+from src import CmdCtl as Cmd
+from src.ocr_utils import OCRUtils as OCR
+from apps.dde_autotest_euler.case.base_case import BaseCase
+from apps.dde_autotest_euler.method.dde_method import DdeMethod
+
+
+class TestDdeCase(BaseCase):
+
+    def test_dde_1952383(self):
+        """启动器打开日历"""
+        euler = DdeMethod()
+        euler.open_software_by_launcher("rili")
+        sleep(3)
+        self.assert_ocr_exist("搜索日程")
+        euler.dde_dock.click_by_ocr("搜索日程")
+        sleep(2)
+        Src.input_message("chuxi")
+        Src.enter()
+        self.assert_ocr_exist("除夕")
+
+
+    @pytest.fixture(autouse=True)
+    def clear(self):
+        DdeMethod().kill_process("dde-calendar")
+        yield
+        DdeMethod().kill_process("dde-calendar")
+        DdeMethod().click_restore()
+        DdeMethod().esc()
