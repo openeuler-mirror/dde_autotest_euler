b58c04a5f80f0b3d000f35409873350286672a80
lixintao <lixintao@uniontech.com>
2026-02-27 17:36:21 +0800

    新增用例test_dde_1979807

---
+++ case/test_dde_1979807.py
@@ -0,0 +1,55 @@
+from time import sleep
+from src import Src
+import pytest
+from src import CmdCtl as Cmd
+from src.ocr_utils import OCRUtils as OCR
+from apps.dde_autotest_euler.case.base_case import BaseCase
+from apps.dde_autotest_euler.method.dde_method import DdeMethod
+
+
+class TestDdeCase(BaseCase):
+
+    def test_dde_1979807(self):
+        """启动器打开日历管理-通用设置-星期开始于的选项"""
+        euler = DdeMethod()
+        euler.open_software_by_launcher("rili")
+        sleep(3)
+        res = OCR.ocr(
+            "日程类型",
+            picture_abspath=None,
+            similarity=0.6,
+            return_first=False,
+            lang='ch',
+            max_match_number=1
+        )
+        euler.dde_dock.click_by_img("calendar_manage.png")
+        euler.dde_dock.click_by_ocr("管理")
+        sleep(1)
+        euler.dde_dock.click_by_ocr("每星期开始于")
+        sleep(1)
+        Src.mouse_scroll(-30)
+        res = OCR.ocr(
+            "周日",
+            picture_abspath=None,
+            similarity=0.6,
+            return_first=False,
+            lang='ch',
+            max_match_number=1
+        )
+        if isinstance(res, tuple):
+            euler.dde_dock.click_by_ocr("周日")
+            sleep(1)
+            self.assert_ocr_exist("周一")
+        else:
+            euler.dde_dock.click_by_ocr("周一")
+            sleep(1)
+            self.assert_ocr_exist("周日")
+
+
+    @pytest.fixture(autouse=True)
+    def clear(self):
+        DdeMethod().kill_process("dde-calendar")
+        yield
+        DdeMethod().kill_process("dde-calendar")
+        DdeMethod().click_restore()
+        DdeMethod().esc()
