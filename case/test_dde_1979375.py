e4bee89d962787dbb0a703a85fc86d4697133fad
mataotao <mataotao@uniontech.com>
2026-03-23 16:55:33 +0800

    新增用例case/test_dde_1979375.py

---
+++ case/test_dde_1979375.py
@@ -0,0 +1,54 @@
+from time import sleep
+from src import Src
+import pytest
+from src import CmdCtl as Cmd
+
+from apps.dde_autotest_euler.case.base_case import BaseCase
+from apps.dde_autotest_euler.method.dde_method import DdeMethod
+
+
+class TestDdeCase(BaseCase):
+
+    def test_dde_1979375_1(self):
+        """启动器打开Elementary Perf"""
+        euler = DdeMethod()
+        euler.open_software_by_launcher("Elementary Perf")
+        sleep(5)
+        self.assert_process_status(True, "elementary_perf")
+
+    def test_dde_1979375_2(self):
+        """桌面打开Elementary Perf"""
+        euler = DdeMethod()
+        euler.dde_dock.click_launcher_btn_by_attr()
+        euler.dde_launcher.click_search_box_by_attr()
+        Src.input("Elementary Perf")
+        sleep(3)
+        euler.dde_launcher.right_click_by_attr("Elementary Perf")
+        Src.select_menu(2)
+        sleep(1)
+        Src.esc()
+        euler.click_restore()
+        sleep(1)
+        euler.dde_dock.double_click_by_img("Elementary_Perf_icon.png")
+        sleep(5)
+        self.assert_process_status(True, "elementary_perf")
+        DdeMethod().kill_process("elementary_perf")
+        sleep(3)
+        euler.dde_dock.right_click_by_img("Elementary_Perf_icon.png")
+        Src.select_menu(1)
+        sleep(5)
+        self.assert_process_status(True, "elementary_perf")
+
+    def test_dde_1979375_3(self):
+        """终端打开Elementary Perf"""
+        Cmd.run_cmd("elementary_perf &")
+        sleep(5)
+        self.assert_process_status(True, "elementary_perf")
+
+    @pytest.fixture(autouse=True)
+    def clear(self):
+        DdeMethod().kill_process("elementary_perf")
+        yield
+        DdeMethod().kill_process("elementary_perf")
+        DdeMethod().click_restore()
+        DdeMethod().esc()
