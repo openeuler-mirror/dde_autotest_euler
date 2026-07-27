394e8ed2de9bd35766c36ee380b127d0b231d063
zhangmeian <zhangmei@uniontech.com>
2026-07-27 13:35:55 +0800

    add testcase 1809179.ts

---
+++ midscene/midscene_dde_file_manager/smoke/vault/1809179.ts
@@ -0,0 +1,112 @@
+
+/**
+ * 用例 PMSID: 1809179
+ * 用例标题: 【保险箱】保险箱入口右键菜单-检查计算机页面/侧边栏右键菜单
+ * 生成时间: 2026-01-15 20:17:30
+ * 用例编写人：UT006252(杨通)
+ */
+
+describe('1809179-【保险箱】保险箱入口右键菜单-检查计算机页面/侧边栏右键菜单', () => {
+  beforeAll(async ({ device, uos, agent,system }) => {
+    console.log('1. beforeAll: 初始化测试套件');
+    await uos.showDesktop();
+    //打开文件管理器并全屏显示
+    await uos.showDesktop();
+    console.log('打开文件管理器');
+    await device.pressKey('Super+E');
+    await system.exec(`sleep 3`);
+    console.log('全屏显示文件管理器');
+    await uos.maximizeWindow();
+    //前置进行保险箱删除操作确保环境干净
+    const TEST_USERNAME = process.env.TEST_USERNAME;
+    await system.exec(`rm -r /home/${TEST_USERNAME}/.config/Vault`);
+    await system.exec(`rm /home/${TEST_USERNAME}/recoveryKey.key`);
+    const result = await system.exec(`fusermount -u /home/${TEST_USERNAME}/.config/Vault/vault_unlocked`);
+    console.log(result.stderr);
+    if (result.success) {
+      console.log('保险箱环境有残留，已强制卸载');
+      await system.exec(`rm -r /home/${TEST_USERNAME}/.config/Vault`);
+      await system.exec(`rm /home/${TEST_USERNAME}/recoveryKey.key`);
+    } else {
+        console.log('保险箱环境无需清理');
+      }
+  });
+
+  beforeEach(async ({ device, agent }) => {
+    console.log('2. beforeEach: 每个测试前的准备');
+    //密匙加密创建保险箱
+    const TEST_PASSWORD = process.env.TEST_PASSWORD;
+    console.log('使用密匙加密创建保险箱');
+    await agent.aiDoubleClick('保险箱图标');
+    await agent.aiWaitFor('出现专属与个人的安全空间文本');
+    await agent.aiTap('开启按钮');
+    await agent.aiInput('Aa!12345','密码输入框');
+    await agent.aiInput('Aa!12345','重复密码输入框');
+    await agent.aiTap('下一步按钮')
+    await agent.aiTap('选择密匙文件保存路径文本右侧的蓝色底三个白色圆点的按钮');
+    await agent.aiTap('保存按钮');
+    await agent.aiTap('下一步按钮');
+    await agent.aiTap('加密保险箱按钮');
+    await device.typeText(`${TEST_PASSWORD}`, true);
+    await agent.aiWaitFor('确定按钮',{ timeoutMs: 60000 });
+    await agent.aiAssert('出现加密已完成文本');
+    console.log('保险箱创建成功');
+    await agent.aiTap('确定按钮');
+  });
+
+  test('1809179-【保险箱】保险箱入口右键菜单-检查计算机页面/侧边栏右键菜单', async ({ device, agent, uos}) => {
+    const TEST_PASSWORD = process.env.TEST_PASSWORD;
+    
+    //上锁保险箱
+    console.log('上锁保险箱');
+    await agent.aiRightClick("保险箱选项");
+    await agent.aiWaitFor("出现菜单栏");
+    await agent.aiTap('立即上锁选项');
+    await agent.aiWaitFor('我的目录文本');
+    await agent.aiRightClick('保险箱图标');
+    await agent.aiWaitFor('出现菜单');
+    await agent.aiAssert("菜单包含解锁、重置密码");
+    await agent.aiTap('侧边栏的保险箱选项');
+
+    //查看右键菜单并删除保险箱
+    await agent.aiRightClick('侧边栏的保险箱选项')
+    await agent.aiWaitFor('出现菜单');
+    await agent.aiAssert("菜单包含解锁、重置密码");
+    await agent.aiTap('解锁选项');
+    await agent.aiInput('Aa!12345','密码输入框');
+    await agent.aiTap('解锁按钮');
+    await agent.aiWaitFor('出现文件夹为空文本');
+    await agent.aiRightClick('保险箱文本');
+    await agent.aiTap('删除保险箱选项');
+    await agent.aiAssert("界面出现删除保险箱后，其中的所有内容都将会被永久删除文本");
+    await agent.aiTap('密码文本') 
+    await device.typeText('Aa!12345', true);
+    await device.typeText(`${TEST_PASSWORD}`);
+    await agent.aiTap('确定按钮')
+    await agent.aiTap('确定按钮')
+   
+  }, { timeout: 1200000, tags: ['1809179', 'level2','smoke','DITT','yangtong'] });
+
+  afterEach(async ({ device,system}) => {
+    console.log('4. afterEach: 每个测试后的清理');
+    const TEST_USERNAME = process.env.TEST_USERNAME;
+    const result = await system.exec(`rm /home/${TEST_USERNAME}/recoveryKey.key`);
+    if (result.success) {
+      console.log('删除密匙文件成功');
+    } else {
+        console.error('错误:', result.stderr);
+      }
+  });
+
+  afterAll(async ({ uos, agent, device, system }) => {
+    console.log('5. afterAll: 清理测试套件');
+    console.log('关闭文件管理器');
+    await uos.closeCurrentWindow();
+    //双重保险清理文件管理器环境
+    await system.exec('killall dde-file-manager');
+    await system.exec('rm -rf .config/deepin/dde-file-manager/dde-file-manager.json');
+    await system.exec('rm -rf dde-file-manager.obtusely.json');  
+    await system.exec('dde-dconfig reset org.deepin.dde.file-manager'); 
+  });
+});
+
