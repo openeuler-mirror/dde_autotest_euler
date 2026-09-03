af29eaba20575fa4cedd35e9c9c69c1fa389e64f
chengweibin <chengweibin@uniontech.com>
2026-09-03 16:45:06 +0800

    添加UI测试脚本：脚本名称为 1583271.ts

---
+++ midscene/midscene_deepin_system_monitor/V25/AI_all/1583271.ts
@@ -0,0 +1,43 @@
+// import "dotenv/config";
+// import { describe, test } from "midscene-uos";
+
+/**
+ * 用例 PMSID: 1583271
+ * 用例标题:等保-自动报警开关默认为关闭状态
+ * 生成时间: 2025-11-19 09:50:26
+ * 用例编写人：ut004526
+ */
+
+describe('1583271-等保-自动报警开关默认为关闭状态', () => {
+    beforeAll(async ({ device, uos, agent ,system}) => {
+      console.log('1. beforeAll: 初始化测试套件');
+      await system.exec('killall deepin-system-monitor')
+      await uos.showDesktop();
+    });
+  
+    beforeEach(async ({ device, agent ,uos}) => {
+      console.log('2. beforeEach: 每个测试前的准备'); 
+      // 前置步骤 1: 打开系统监视器
+      await uos.openApp("系统监视器", 2000, 40000,false);
+      console.log('beforeEach done')
+    });
+  
+    test('1583271-等保-自动报警开关默认为关闭状态', async ({ device, agent, uos}) => {
+      // 步骤 1: 点击主菜单设置，检查系统防护中的自动报警开关
+      await agent.aiTap("窗口标题栏的三条横线图标");
+      await agent.aiTap("设置");
+      // 验证：自动报警开关默认关闭状态，系统防护区域为置灰状态
+      await agent.aiAssert("'自动报警'后面的按钮不是蓝色");
+      await agent.aiAssert("'自动报警'下面三行字体都是浅灰色");
+    }, { timeout: 600000, tags: ['1583271','level1','smoke'] });  
+  
+    afterEach(async ({ device,agent }) => {
+      console.log('4. afterEach: 每个测试后的清理');
+    });
+  
+    afterAll(async ({ uos, agent, device, system }) => {
+      console.log('5. afterAll: 清理测试套件');
+      await device.pressKey("ESC")
+      await agent.aiTap("窗口右上角关闭按钮:X");
+    });
+  });
